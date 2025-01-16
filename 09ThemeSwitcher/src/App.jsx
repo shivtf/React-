578b5e6f70e968737a9fa09de265b470fbaa69db
shivtf <shivam714bhandari@gmail.com>
2025-01-16 10:57:12 +0530

    Updated Code

--- 09ThemeSwitcher/src/App.jsx
+++ 09ThemeSwitcher/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-import './App.css'
+//import './App.css'
 import { ThemeProvider } from './contexts/theme'
 import { useEffect } from 'react'
 import ThemeBtn from './components/ThemeButton'
@@ -10,32 +10,32 @@ function App() {
   //const [count, setCount] = useState(0)
   const [themeMode, setThemeMode] = useState('light')
 
-  const lightTheme = ()=>{
+  const lightTheme = () => {
     setThemeMode('light')
   }
-  const darkTheme = ()=>{
+  const darkTheme = () => {
     setThemeMode('dark')
   }
 
   // actual theme change
-  useEffect(()=>{
-    document.querySelector('html').classList.remove('light','dark')
-    document.querySelector('html').classList.add('themeMode')
-  },[themeMode])
+  useEffect(() => {
+    document.querySelector('html').classList.remove('light', 'dark')
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
 
   return (
-    <ThemeProvider value={{themeMode,darkTheme,lightTheme}}>
-    <div className="flex flex-wrap min-h-screen items-center">
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
-            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                < ThemeBtn/>
-            </div>
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            < ThemeBtn />
+          </div>
 
-              <div className="w-full max-w-sm mx-auto">
-                <Card />
-              </div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
         </div>
-    </div>
+      </div>
     </ThemeProvider>
   )
 }
